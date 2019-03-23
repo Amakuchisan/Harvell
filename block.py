cf635738ebf7a3efd07892c9e29c966729f0a39c
Amakuchisan <s18t318@stu.kagawa-u.ac.jp>
2019-03-23 19:34:42 +0900

    set normal block

---
+++ block.py
@@ -0,0 +1,19 @@
+class Vec2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Block:
+    def __init__(self, img_id):
+        self.pos = Vec2(60, 80)
+        self.img_player = img_id
+
+    def update_floor(self, x, y):
+        if (
+            x <= self.pos.x+16
+            and x >= self.pos.x-8
+            and y == self.pos.y-7
+        ):
+            y = self.pos.y
+        else:
+            return True
--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pyxel
 import player
+import block
 
 WINDOW_H = 120
 WINDOW_W = 160
 PLAYER_H = 7
 PLAYER_W = 9
+BLOCK_H = 8
+BLOCK_W = 16
 
 class App:
     def __init__(self):
         self.IMG_PLAYER = 0
+        self.IMG_BLOCK = 0
 
         pyxel.init(WINDOW_W, WINDOW_H, caption="Harvell")
         pyxel.load("assets/my_resource.pyxel")
 
         self.mplayer = player.Player(self.IMG_PLAYER)
+        self.block = block.Block(self.IMG_BLOCK)
 
         pyxel.run(self.update, self.draw)
 
@@ -30,7 +35,8 @@ class App:
         if pyxel.btn(pyxel.KEY_SPACE):
             self.mplayer.jump(1)
         elif self.mplayer.pos.y < 105: # 105 is initial height, player should be stop on block
-            self.mplayer.jump(-2)
+            if self.block.update_floor(self.mplayer.pos.x, self.mplayer.pos.y):
+                self.mplayer.jump(-1)
 
         self.mplayer.update(dx)
 
@@ -41,9 +47,13 @@ class App:
         # pyxel.bltm(self.mplayer.pos.x, 0, 0, 0, 0, 30, 16,) # how to scroll
         pyxel.bltm(0, 0, 0, 0, 0, 30, 16,)
 
+        # draw block
+        pyxel.blt(self.block.pos.x, self.block.pos.y, self.IMG_BLOCK, 16, 16, BLOCK_W, BLOCK_H, )
+
         # draw player
         # pyxel.blt(self.mplayer.pos.x, self.mplayer.pos.y, self.IMG_PLAYER, 0, 0, PLAYER_W, PLAYER_H, 4) # big
         pyxel.blt(self.mplayer.pos.x, self.mplayer.pos.y, self.IMG_PLAYER, 16, 9, PLAYER_W, PLAYER_H, 4) # small
+
 
 if __name__ == "__main__":
     App()
